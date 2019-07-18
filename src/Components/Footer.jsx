412d73c984e2971afb8d0d7a6f232d978a04bcce
ashler2 <ashley.rt@live.co.uk>
2019-07-18 16:29:02 +0100

    fixed the security threat with html link

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,15 +4,20 @@ import linkedIn from "../images/linkedIn.png";
 const Footer = () => {
   return (
     <div className="Footer">
-      <a href="https://github.com/ashler2" target="_blank">
-        <img src={gitHub} />
+      <a
+        rel="noopener noreferrer"
+        href="https://github.com/ashler2"
+        target="_blank"
+      >
+        <img src={gitHub} alt="link to Github" />
       </a>
       <p>By: Ashley Readman-Thorley</p>
       <a
+        rel="noopener noreferrer"
         href="https://www.linkedin.com/in/ashley-readman-thorley-baa0bb139/"
         target="_blank"
       >
-        <img src={linkedIn} />
+        <img src={linkedIn} alt="Link to linkedIn" />
       </a>
     </div>
   );
